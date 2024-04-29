016214d92ff02e890d07054106f38ca771317a45
vaishali546 <152677890+vaishali546@users.noreply.github.com>
2024-04-29 06:42:08 +0000

    datatypes and all

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-/*console.log("hello there")
+console.log("hello there")
 console.log(typeof null)
 console.log(typeof undefined)
 let score = 24
@@ -14,7 +14,23 @@ console.log(Boolean("ahhl"))
 console.log(typeof(String(33)))
 console.log(typeof(String(null)))
 console.log(typeof(String(undefined)))
-console.log(String(-7))*/
-
-
-
+console.log(String(-7))
+console.log("2"==2)
+console.log("2"===2)
+let id = Symbol("123")
+let anotherId = Symbol("123")
+console.log(id == anotherId)
+console.log(id === anotherId)
+let bigNumber = 895791258n
+console.log(typeof bigNumber);
+let arr = ["first","second"]
+let myobj ={
+    name:"vaishali",
+    age:4,
+}
+const func = function(){
+    console.log("Hello World")
+}
+console.log(typeof arr,typeof myobj, typeof func)
+console.log(arr,myobj)
+func()
