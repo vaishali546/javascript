5224b533a3e74ac815d2ad0051f514cb35d4f8c7
vaishali546 <152677890+vaishali546@users.noreply.github.com>
2024-04-29 05:13:17 +0000

    conversions

--- test.js
+++ test.js
@@ -1,1 +1,20 @@
-console.log("hello there")
+/*console.log("hello there")
+console.log(typeof null)
+console.log(typeof undefined)
+let score = 24
+console.log(typeof score)
+console.log(Boolean(1))
+console.log(Boolean(0))
+console.log(Boolean(3))
+console.log(Boolean(-7))
+console.log(Boolean(null))
+console.log(Boolean(undefined))
+console.log(Boolean(""))
+console.log(Boolean("ahhl"))
+console.log(typeof(String(33)))
+console.log(typeof(String(null)))
+console.log(typeof(String(undefined)))
+console.log(String(-7))*/
+
+
+
